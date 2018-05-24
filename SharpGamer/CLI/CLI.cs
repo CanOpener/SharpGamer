3d46322fb7826e9d54264c29c4e7fc2389341e05
Mladen Kajic <mladenkajic96@gmail.com>
2018-05-24 13:56:35 +0100

    Simulation engine, 2 Main entry points and APIs namespace

--- SharpGamer/CLI/CLI.cs
+++ SharpGamer/CLI/CLI.cs
@@ -4,6 +4,11 @@ using System.Text;
 
 namespace SharpGamer.CLI
 {
+    /*
+     * This is generally for debugging the different
+     * the different parts of the project. Uses consol
+     * for quick and easy changes.
+    */
     class CLI
     {
         static void Main(string[] args)
