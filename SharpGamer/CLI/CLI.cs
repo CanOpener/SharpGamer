490db60c56b666196f08a7e4c0f60ec637202b47
Mladen Kajic <mladenkajic96@gmail.com>
2018-05-24 13:38:12 +0100

    CLI skeleton

---
+++ SharpGamer/CLI/CLI.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharpGamer.CLI
+{
+    class CLI
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CLI Started");
+        }
+    }
+}
